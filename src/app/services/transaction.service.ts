f46c857fbc1bc1006199ed5079bb0290af02c8cc
JoseMartins0808 <ottenazuose@hotmail.com>
2024-05-07 12:55:53 -0300

    feat: total transactions value method

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,13 +1,30 @@
-import { Injectable, signal } from "@angular/core";
+import { Injectable, computed, signal } from "@angular/core";
 import { ICreateTransaction, ITransaction } from "../interfaces/transaction.interfaces";
 
 @Injectable({ providedIn: "root" })
 export class TransactionService {
+
     private readonly transactionList = signal<ITransaction[]>([]);
+
+    private readonly totalTransactionsValue = computed(() => this.transactionList()
+        .reduce((previousValue, currentvalue) => {
+
+            if (currentvalue.type === "input") {
+                return previousValue + Number(currentvalue.value);
+
+            } else {
+                return previousValue - Number(currentvalue.value);
+            }
+        }, 0));
 
     public getTransactionList() {
 
-        return this.transactionList;
+        return this.transactionList();
+    }
+
+    public getTotalTransactionsValue() {
+
+        return this.totalTransactionsValue();
     }
 
     public addTransaction(formData: ICreateTransaction) {
