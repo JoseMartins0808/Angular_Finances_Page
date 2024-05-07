d1bb0cf9b2866ca399b8b28d087e8826b69c5158
JoseMartins0808 <ottenazuose@hotmail.com>
2024-05-07 19:34:28 -0300

    feat: ng-for transactions cards

--- src/app/components/transaction-card/transaction-card.component.ts
+++ src/app/components/transaction-card/transaction-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ITransaction } from '../../interfaces/transaction.interfaces';
 
 @Component({
   selector: 'app-transaction-card',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './transaction-card.component.sass'
 })
 export class TransactionCardComponent {
-
+  @Input() transaction!: ITransaction;
 }
--- src/app/pages/transactions/transactions.component.ts
+++ src/app/pages/transactions/transactions.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { RegisterTransactionFormComponent } from '../../components/forms/register-transaction-form/register-transaction-form.component';
 import { TotalDisplayComponent } from '../../components/total-display/total-display.component';
 import { TransactionCardComponent } from '../../components/transaction-card/transaction-card.component';
+import { CommonModule } from '@angular/common';
+import { TransactionService } from '../../services/transaction.service';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [RegisterTransactionFormComponent, TotalDisplayComponent, TransactionCardComponent],
+  imports: [CommonModule, RegisterTransactionFormComponent, TotalDisplayComponent, TransactionCardComponent],
+  providers: [TransactionService],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.sass'
 })
 export class TransactionsComponent {
 
+  constructor(private readonly transactionService: TransactionService) { };
+
+  get transactionList() {
+    return this.transactionService.getTransactionList();
+  }
 }
